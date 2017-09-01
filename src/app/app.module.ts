97379380255141220a87c5d7ce111f9d3e249fee
juan Pc/Miga <ing.juan.garfias@gmail.com>
2017-09-01 18:27:01 -0500

    Parcial 6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard.components';
+import { DashboardComponent } from './dashboard.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
