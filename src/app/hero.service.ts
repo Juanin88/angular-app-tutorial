19b7413886e8447a5c039bde74e6721b1043a99b
juan Pc/Miga <ing.juan.garfias@gmail.com>
2017-09-05 13:56:51 -0500

    Delete Heroe.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -55,4 +55,14 @@ export class HeroService {
     .catch(this.handleError);
   }
 
+  delete(id: number): Promise<void> {
+  const url = `${this.heroesUrl}/${id}`;
+    console.log(this.heroesUrl);
+    console.log(url);
+  return this.http.delete(url, {headers: this.headers})
+    .toPromise()
+    .then(() => null)
+    .catch(this.handleError);
+  }
+
 }
